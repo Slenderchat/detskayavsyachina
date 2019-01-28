84ed8260454862c96a887e718fb51987994ba165
Slenderchat <steeplike@gmail.com>
2019-01-28 16:04:34 +0300

    Overhaul

--- includes/menu.php
+++
@@ -1,6 +0,0 @@
-<a href="/">Главная</a>
-<a  href="/novosti">Новости</a>
-<a  href="/faq">FAQ</a>
-<!--<a  href="/otzyvy">Отзывы</a>!-->
-<a  href="/prokat">Прокат</a>
-<a  href="/prodazha">Продажа</a>
